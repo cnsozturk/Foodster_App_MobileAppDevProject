726c1bec05a1e65749741b5387ccf18ea895f3ad
allenfjd <allenfjd@gmail.com>
2023-03-14 22:33:47 -0700

    Changes to login, creation of users restricted by user names

--- app/src/main/java/com/example/foodster_app_mobileappdevproject/RestaurantRegisterActivity.java
+++ app/src/main/java/com/example/foodster_app_mobileappdevproject/RestaurantRegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.foodster_app_mobileappdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -29,6 +31,7 @@ public class RestaurantRegisterActivity extends AppCompatActivity {
 
         buttonRegisterRestaurant.setOnClickListener(new View.OnClickListener() {
             boolean isRegistered;
+            boolean userFoundInCustomerTable = false;
             @Override
             public void onClick(View view) {
                 String chosenCityFromSpinner = spinnerRestaurantCity.getSelectedItem().toString();
@@ -43,28 +46,43 @@ public class RestaurantRegisterActivity extends AppCompatActivity {
                     Toast.makeText(RestaurantRegisterActivity.this, "Please fill all the fields", Toast.LENGTH_LONG).show();
                 }
                 else {
-                    isRegistered = dbh.addDataRestaurantTable(editTextRestaurantUserNameEmail.getText().toString(),
-                            editTextRestaurantPassword.getText().toString(),
-                            editTextNameOfRestaurant.getText().toString(),
-                            editTextFirstNameRestaurant.getText().toString(),
-                            editTextLastNameRestaurant.getText().toString(),
-                            editTextPhoneRestaurant.getText().toString(),
-                            chosenCityFromSpinner,
-                            editTextAddressRestaurant.getText().toString());
-                    if(isRegistered){
-                        Toast.makeText(RestaurantRegisterActivity.this, "Restaurant Registered Successfully", Toast.LENGTH_LONG).show();
-                        editTextRestaurantUserNameEmail.setText("");
-                        editTextRestaurantPassword.setText("");
-                        editTextNameOfRestaurant.setText("");
-                        editTextFirstNameRestaurant.setText("");
-                        editTextLastNameRestaurant.setText("");
-                        editTextPhoneRestaurant.setText("");
-                        spinnerRestaurantCity.setSelection(0);
-                        editTextAddressRestaurant.setText("");
+                    Cursor cursorCustomerTable = dbh.viewDataFromCustomerTable();
+                    if (cursorCustomerTable.getCount() > 0)
+                        if (cursorCustomerTable.getCount() > 0){
+                            while (cursorCustomerTable.moveToNext()) {
+                                if (editTextRestaurantUserNameEmail.getText().toString().equals(cursorCustomerTable.getString(0))){
+                                    userFoundInCustomerTable = true;
+                                }
+                            }
+                        }
+                    if(userFoundInCustomerTable){
+                        Toast.makeText(RestaurantRegisterActivity.this, "Restaurant Registration unsuccessful, User Email already registered as customer", Toast.LENGTH_LONG).show();
                     }
-                    else{
-                        Toast.makeText(RestaurantRegisterActivity.this, "Restaurant Registration unsuccessful, Restaurant User Email and Restaurant Name already registered", Toast.LENGTH_LONG).show();
-                    }
+                    else {
+                            isRegistered = dbh.addDataRestaurantTable(editTextRestaurantUserNameEmail.getText().toString(),
+                                    editTextRestaurantPassword.getText().toString(),
+                                    editTextNameOfRestaurant.getText().toString(),
+                                    editTextFirstNameRestaurant.getText().toString(),
+                                    editTextLastNameRestaurant.getText().toString(),
+                                    editTextPhoneRestaurant.getText().toString(),
+                                    chosenCityFromSpinner,
+                                    editTextAddressRestaurant.getText().toString());
+                            if(isRegistered){
+                                Toast.makeText(RestaurantRegisterActivity.this, "Restaurant Registered Successfully", Toast.LENGTH_LONG).show();
+                                editTextRestaurantUserNameEmail.setText("");
+                                editTextRestaurantPassword.setText("");
+                                editTextNameOfRestaurant.setText("");
+                                editTextFirstNameRestaurant.setText("");
+                                editTextLastNameRestaurant.setText("");
+                                editTextPhoneRestaurant.setText("");
+                                spinnerRestaurantCity.setSelection(0);
+                                editTextAddressRestaurant.setText("");
+                            }
+                            else{
+                                Toast.makeText(RestaurantRegisterActivity.this, "Restaurant Registration unsuccessful, Restaurant User Email and Restaurant Name already registered", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
                 }
 
 
